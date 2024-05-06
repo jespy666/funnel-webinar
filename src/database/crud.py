8cdc88a95a509d58d74891020e297f67d798564d
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-05-06 23:34:47 +0500

    get user by multi statuses

--- src/database/crud.py
+++ src/database/crud.py
@@ -1,8 +1,8 @@
 from collections.abc import Sequence
+from typing import List
 
-from sqlalchemy import select, delete
+from sqlalchemy import select, delete, or_, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm.exc import NoResultFound
 
 from .session import AsyncSessionFactory
 from .models import User
@@ -31,24 +31,21 @@ class CRUD(AsyncSessionFactory):
         await session.close()
         return user
 
-    async def get_users_by_status(self, status: str) -> Sequence[User]:
-        stmt = select(User).filter(User.status == status)
+    async def get_users_by_status(self, statuses: List[str]) -> Sequence[User]:
+        stmt = select(User).filter(or_(
+            *[User.status == status for status in statuses]
+        ))
         session: AsyncSession = await super().get_session()
         result = await session.execute(stmt)
         users: Sequence[User] = result.scalars().all()
         await session.close()
         return users
 
-    async def update_status(self, pk: int, status: str) -> None:
-        stmt = select(User).where(User.id == pk)
+    async def update_user(self, user_id: int, **kwargs) -> None:
+        stmt = update(User).where(User.id == user_id).values(**kwargs)
         session: AsyncSession = await super().get_session()
-        result = await session.execute(stmt)
-        user: User | None = result.scalar_one_or_none()
-        if user:
-            user.status = status
-            await session.commit()
-        else:
-            raise NoResultFound('Пользователь с таким ID не найден')
+        await session.execute(stmt)
+        await session.commit()
         await session.close()
 
     async def delete_user(self, pk: int) -> None:
